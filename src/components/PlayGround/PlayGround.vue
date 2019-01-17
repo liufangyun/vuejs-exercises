b8ba2ae045a5e916ebb4b2cf69944cb963e56bce
Fangyun,Liu <liu00433@algonquinlive.com>
2019-01-17 11:57:57 -0500

    initializing PlayGround with Separation of Concerns

---
+++ src/components/PlayGround/PlayGround.vue
@@ -0,0 +1,18 @@
+<script src="./PlayGround.js"></script>
+<style src="./PlayGround.css" scoped></style>
+
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayGround',
+}
+</script>
+
+
+
+
